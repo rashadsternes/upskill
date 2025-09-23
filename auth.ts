19d2f2132bc9c5ee977fb2272e9f28db78190f4a
rashadsternes <nrsternes@gmail.com>
2025-09-23 11:30:29 -0500

    Setup OAuth with Github

--- auth.ts
+++ auth.ts
@@ -1,9 +1,6 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import Apple from "next-auth/providers/apple"
-import Instagram from "next-auth/providers/instagram"
-
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Apple, Instagram],
+  providers: [GitHub],
 });
