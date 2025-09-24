2cad4ac6d8b4cf84badd769d5a231e2fcb5d2884
rashadsternes <nrsternes@gmail.com>
2025-09-23 23:04:14 -0500

    Implement search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({ searchParams}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
     <section className="pink_container">
       <h1 className="heading">Pitch Your Startup, <br />
       Connect With Entreprenuers</h1>
-      <p className="sub-heaing !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+      <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+      
+      <SearchForm query={query} />
     </section>
     </>
   );
