7abd34a2c22d5bda90d932097e2ca7035e3278df
rashadsternes <nrsternes@gmail.com>
2025-09-25 13:03:56 -0500

    Create 1st Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams}: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: "Filiz Elaerts"},
+    _id: 1,
+    description: " This is a descriptiion",
+    image: "https://images.unsplash.com/photo-1610375229632-c7158c35a537",
+    category: "Mountains",
+    title: "Mount Fuji",
+  }];
   return (
     <>
     <section className="pink_container">
@@ -13,6 +24,21 @@ export default async function Home({ searchParams}: {
       <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
       
       <SearchForm query={query} />
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : 'All Startups'}
+      </p>
+
+      <ul className="mt-7 card_grid">
+        {posts?.length > 0 ? (
+          posts.map((post:StartupCardType) => (
+            <StartupCard key={post?.id} post={post} />
+          ))
+        ) : (
+          <p className="no-results">No startups found</p>
+        )}
+      </ul>
     </section>
     </>
   );
