2e5fa4442b701dba0d8e82b8c5ee4b9f3178e55e
rashadsternes <nrsternes@gmail.com>
2025-10-02 13:57:50 -0500

    Implement View incrementor w/ Sanity

--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,18 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
-  // TODO: Update number of views
+
+  await writeClient
+    .patch(id)
+    .set({ views: (totalViews || 0) + 1 })
+    .commit();
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
